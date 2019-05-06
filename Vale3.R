8486dc2f7a55b031a498705731f73b6f6d75cd4c
Valmor Mantelli Jr <44906350+Valmorj@users.noreply.github.com>
2019-05-06 15:41:39 -0300

    Add files via upload
    Ajuste dia 06/05

--- Vale3.R
+++ Vale3.R
@@ -3,39 +3,40 @@ install.packages('ggplot2')
 install.packages('ggthemes')
 install.packages('scales')
 install.packages('tseries')
-install.packages('graphics')
 install.packages('quantmod')
 install.packages('dgof')
 install.packages('qqnorm')
 install.packages('ggthemes')
+install.packages('zoo')
+install.packages('xts')
 
 library(tseries)
-library(graphics)
 library(dgof)
 library(qqnorm)
 library(quantmod)
 library(ggplot2)
 library(scales)
 library(ggthemes)
+library(zoo)
+library(xts)
 
 #Carregando a ação e plotando o gráfico
 env = new.env()
 getSymbols("VALE3.SA",src="yahoo", 
 env=env,
-from=as.Date('2018-01-14'))
+from=as.Date('2018-01-02'))
 vale = env$VALE3.SA[,4]
 
 autoplot(vale)
 
+#Selecionando valores
+tam <-length(vale)
+pri <- vale [1]
+ult <- vale[tam]
+
 #Medidas de variabilidade
 media <- mean(vale)
 desvio <- sd(vale)
-
-#Normal
-tamanho <- seq(media-4*desvio, media+4*desvio, 0.01)
-nor <- dnorm(tamanho, media, desvio)
-plot (tamanho, nor, main = , type = 'l', ylim = c(0,max(nor)+0.01), axes = FALSE)
-axis (4, at = seq(media - 3*desvio, media + 3*desvio, desvio))
 
 #Grafico do quantil
 qqnorm(vale)
