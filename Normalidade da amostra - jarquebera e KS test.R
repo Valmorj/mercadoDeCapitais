fe0e8dfc01365a935482b4bf2745455764acc34b
Valmor Mantelli Jr <44906350+Valmorj@users.noreply.github.com>
2019-05-13 23:01:02 -0300

    Análise de normalidade de uma ação - Testes Jarque Bera e KS

---
+++ Normalidade da amostra - jarquebera e KS test.R
@@ -0,0 +1,61 @@
+#Instalando pacotes e e carregando
+install.packages('ggplot2')
+install.packages('ggthemes')
+install.packages('scales')
+install.packages('tseries')
+install.packages('graphicsQC')
+install.packages('quantmod')
+install.packages('dgof')
+install.packages('ggthemes')
+install.packages('zoo')
+install.packages('KScorrect')
+
+library(tseries)
+library(graphics)
+library(dgof)
+library(qqnorm)
+library(quantmod)
+library(ggplot2)
+library(scales)
+library(ggthemes)
+library(zoo)
+library(xts)
+library(KScorrect)
+
+#Carregando a ação e plotando o gráfico
+env = new.env()
+getSymbols("VALE3.SA",src="yahoo", 
+           env=env,
+           from=as.Date('2018-01-02'))
+vale = env$VALE3.SA[,4]
+
+autoplot(vale)
+
+datavale <- data.frame(vale)
+
+#Selecionando valores
+tam <-length(vale)
+pri <- datavale [1,1]
+ult <- datavale [tam,1]
+
+#Medidas de variabilidade
+media <- mean(vale)
+desvio <- sd(vale)
+dist <- rnorm(tam, media, desvio)
+min <- min(vale)
+max <- max(vale)
+
+#Rentabilidade
+##Rentabilidade no período
+rp <- (ult/pri) - 1
+##Rentabilidade anual
+ra <- (1 + rp)^(252/tam) - 1
+
+#Teste Jarque Bera
+jarque.bera.test(vale)
+
+#Teste Kolmogorov-Smirnov(KS)
+ks.test(vale, pnorm, mean(vale), sd(vale)) 
+LcKS(vale, cdf = "pnorm")
+
+
